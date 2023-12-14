247eab392f1d0aa31798c97b8b389b31eb05e916
Hamza Ahmed <kojozcodes@gmail.com>
2023-12-14 21:55:20 +0100

    modify divide function and define operate function

--- script.js
+++ script.js
@@ -12,5 +12,33 @@ const multiply = (num1,num2) => {
 }
 
 const divide = (num1,num2) => {
-    return num1 / num2
+    if (num2 !== 0) {
+        return num1/num2
+    } else {
+        return "Cannot divide by zero"
+    }
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+const operate = (num1, op, num2) => {
+    switch (op) {
+        case '+':
+            add(num1, num2)
+            break;
+        
+        case '-':
+            subtract(num1, num2)
+            break;
+
+        case '*':
+            multiply(num1, num2)
+            break;
+
+        case '/':
+            divide(num1, num2)
+            break;
+    }
 }

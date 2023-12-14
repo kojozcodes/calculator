72359d81b343df51038745cd0602be53b07cd2b5
Hamza Ahmed <kojozcodes@gmail.com>
2023-12-14 23:08:46 +0100

    define functions that update the display of the solution

--- script.js
+++ script.js
@@ -19,9 +19,49 @@ const divide = (num1,num2) => {
     }
 }
 
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+
+let display = document.querySelector('#cal-display')
+
+const appendNumber = (num) => {
+    if (operator === '') {
+        firstNumber += num
+        updateDisplay(firstNumber)
+    } else {
+        secondNumber += num
+        updateDisplay(secondNumber)
+    }
+}
+
+const setOperator = (op) => {
+    if (firstNumber !== '' && secondNumber === '') {
+       operator = op
+       updateDisplay(`${firstNumber} ${operator}`)
+    }
+}
+
+const clearDisplay = () => {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    updateDisplay('0')
+}
+
+const updateDisplay = (value) => {
+    display.value = value
+}
+
+const calculate = () => {
+    if (firstNumber !== '' && operator !== '' && secondNumber !== '') {
+        const result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber))
+        updateDisplay(result)
+        firstNumber = result.toString();
+        operator = ''
+        secondNumber = ''
+    }
+}
 
 const operate = (num1, op, num2) => {
     switch (op) {
